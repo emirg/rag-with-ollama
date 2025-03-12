433ed3002eae85657e44362c00f72e79aabe5732
Emiliano Rios Gavagnin <emiriosgavag@gmail.com>
2025-03-12 16:56:13 -0500

    🌐 Update API with english comments and env vars

--- app/api/api.py
+++ app/api/api.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel
 from langchain_chroma import Chroma
@@ -5,18 +7,21 @@ from langchain_ollama.embeddings import OllamaEmbeddings
 from langchain.chains import RetrievalQA
 from langchain_ollama import OllamaLLM
 
-CHROMA_DB_DIR = "db_chroma"
+load_dotenv()
+
+CHROMA_DB_DIR =  os.getenv("CHROMA_DB_DIR") 
+LLM_MODEL =  os.getenv("LLM_MODEL")
 
 def initialize_rag():
-    embedding_function = OllamaEmbeddings(model="mistral")
+    embedding_function = OllamaEmbeddings(model=LLM_MODEL)
     vectorstore = Chroma(persist_directory=CHROMA_DB_DIR, embedding_function=embedding_function)
     retriever = vectorstore.as_retriever()
-    # Configurar la cadena de QA
-    qa_chain = RetrievalQA.from_chain_type(OllamaLLM(model="mistral"), retriever=retriever)
+    # Configure document retriever
+    qa_chain = RetrievalQA.from_chain_type(OllamaLLM(model=LLM_MODEL), retriever=retriever)
     return qa_chain
 
 # Initialize FastAPI
-app = FastAPI(title="RAG API", description="API para consultar documentos con RAG", version="1.0")
+app = FastAPI(title="RAG API", description="API for querying documents with RAG", version="1.0")
 
 qa_chain = initialize_rag()
 
@@ -26,13 +31,12 @@ class QueryRequest(BaseModel):
 
 @app.post("/query/")
 async def ask_question(request: QueryRequest):
-    """Recibe una pregunta y devuelve la respuesta basada en los documentos indexados."""
     response = qa_chain.invoke(request.question)
     return {"question": request.question, "answer": response['result']}
 
 @app.get("/")
 async def root():
-    return {"message": "Bienvenido a la API de RAG. Usa /query/ para hacer preguntas."}
+    return {"message": "Welcome to a RAG API. Use /query/."}
 
 
 
